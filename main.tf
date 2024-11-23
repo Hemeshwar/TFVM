85c2dc355601490f46ae2c7ffbeb6b785211f1b6
Hemeshwar <hemeshwar.sivaram>
2024-11-23 14:13:22 +0530

    second commit

--- main.tf
+++ main.tf
@@ -1,17 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  subscription_id = "6d8cb5f9-d67d-4672-9291-01934fa74bc5"
-  features {}
-}
 
 resource "azurerm_resource_group" "terra-demo" {
   name     = "terra-demo-rg"
